14807ed901c22eebbd12821e83f1a9e75f5f13a0
Jeremias Marinho <jeremias10@msn.com>
2021-12-05 00:06:26 -0300

    two commit

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 
-function DadosUsuario({ aoEnviar }) {
+function DadosUsuario({ aoEnviar, validacoes }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+
+  const [erros, setErros] = useState({ senha: { valido: true, texto: "" } });
+
+
+  function validarCampos(event){
+      const {name, value} = event.target;
+      const newState = {...erros};
+      newState[name] = validacoes[name](value);
+      setErros(newState);
+    
+  }
+
   return (
     <form
       onSubmit={(event) => {
@@ -17,6 +29,7 @@ function DadosUsuario({ aoEnviar }) {
           setEmail(event.target.value);
         }}
         id="email"
+        name="email"
         label="email"
         type="email"
         required
@@ -29,7 +42,11 @@ function DadosUsuario({ aoEnviar }) {
         onChange={(event) => {
           setSenha(event.target.value);
         }}
+        onBlur={validarCampos}
+        error={!erros.senha.valido}
+        helperText={erros.senha.texto}
         id="senha"
+        name="senha"
         label="senha"
         type="password"
         required
